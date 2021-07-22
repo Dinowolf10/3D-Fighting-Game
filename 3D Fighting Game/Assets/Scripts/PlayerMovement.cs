6659a05eb2a6aea4b58f4b042725fd9330f74be2
Zaven <dinowolf10@gmail.com>
2021-07-21 22:55:58 -0400

    Added jumping functionality for the player, imported the Y bot model, and hooked up an idle and walking animation

--- 3D Fighting Game/Assets/Scripts/PlayerMovement.cs
+++ 3D Fighting Game/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+
     public Transform playerTransform;
 
     public float speed;
 
+    public float jumpForce;
+
+    public bool isWalking = false;
+
+    public bool isJumping = false;
+
+    public Animator playerAnimator;
+
     // Start is called before the first frame update
     private void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     private void Update()
     {
+        // If the player presses jump and the player is currently not jumping
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            // Call the jump method
+            Jump();
+        }
 
+        // Checks and updates the current actions the player is performing
+        CheckMovement();
     }
 
     private void FixedUpdate()
@@ -40,12 +58,61 @@ public class PlayerMovement : MonoBehaviour
         // If there is input on both the horizontal and vertical axis, half the player speed
         if (horizontal != 0 && vertical != 0)
         {
+            // Move player based on input and speed
             playerTransform.Translate(new Vector3(horizontal / 2, 0, vertical / 2) * speed * Time.fixedDeltaTime);
+
+            // Set is walking to true
+            isWalking = true;
+
+            // Set is walking to true in the player animator controller
+            playerAnimator.SetBool("isWalking", true);
+        }
+        // If the player is not moving
+        else if (horizontal == 0 && vertical == 0)
+        {
+            // Set is walking to true
+            isWalking = false;
+
+            // Set is walking to true in the player animator controller
+            playerAnimator.SetBool("isWalking", false);
         }
         // Otherwise move the player normally
         else
         {
+            // Move player based on input and speed
             playerTransform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+
+            isWalking = true;
+
+            playerAnimator.SetBool("isWalking", true);
+        }
+    }
+
+    /// <summary>
+    /// Adds force to Y axis the player to simulate a jump
+    /// </summary>
+    private void Jump()
+    {
+        // Adds force to player on the Y axis
+        rb.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+
+        // Set isJumping to true
+        isJumping = true;
+    }
+
+    /// <summary>
+    /// Checks and updates the actions the player is performings
+    /// </summary>
+    private void CheckMovement()
+    {
+        // Checks to see if the player is jumping
+        if (rb.velocity.y != 0)
+        {
+            isJumping = true;
+        }
+        else
+        {
+            isJumping = false;
         }
     }
 }
